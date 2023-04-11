bef8fa5220f99892c2f5cce1816137e1683d9364
Rutvik_patel <rutvikpatel7534@gmail.com>
2023-04-11 17:13:44 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MapDemo from './Components/MapDemo';
-import Todo from './Components/ToDO';
+// import Todo from './Components/ToDO';
+import UserInput from './Components/UserInput';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Todo/>
+    {/* <Todo/> */}
     {/* <App /> */}
+    <UserInput/>
   </React.StrictMode>
 );
 
