21fcd91928939b3a5051e2008b5c1412e1fb8714
Rutvik_patel <rutvikpatel7534@gmail.com>
2023-04-11 16:18:36 +0530

    edited

--- src/Components/ToDO.js
+++ src/Components/ToDO.js
@@ -1,65 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from "react";
 
-function ToDO() {
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
 
-    const [task, setTask] = useState([])
 
-    const handleAddClick = (event) => {
-        event.preventDefault();
-        const NewTask = event.target.task.value;
-        setTask([...task, { text: NewTask, Completed: false }]);
-        localStorage.setItem('tasks', JSON.stringify(NewTask));
-        event.target.reset();
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
     }
+  }, []);
 
-    const handleDeleteTask = (index) => {
-        const newTasks = [...task];
-        newTasks.splice(index, 1);
-        setTask(newTasks);
-    };
-    const handleEditTask = (index, newTask) => {
-        const newTasks = [...task];
-        newTasks[index].text = newTask;
-        setTask(newTasks);
-    };
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
-    const handleToggleComplete = (index) => {
-        const newTasks = [...task];
-        newTasks[index].completed = !newTasks[index].completed;
-        setTask(newTasks);
-    };
 
-    return (
-        <>
-            <form onSubmit={handleAddClick}>
-                <input type="text" name='task' placeholder='Enter Something..' />
-                <button>Add</button>
-            </form>
-            <ul>
-                {task.map((task, index) => (
-                    <li key={index}>
-                        <input
-                            type="checkbox"
-                            checked={task.completed}
-                            onChange={() => handleToggleComplete(task)}
-                        />
-                        {task.text}
-                        <button onClick={() => handleDeleteTask(task)}>Delete</button>
-                        <button
-                            onClick={() =>
-                                handleEditTask(
-                                    task,
-                                    prompt("Enter the new task text:", task.text)
-                                )
-                            }
-                        >
-                            Edit
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    const newTask = event.target.task.value;
+    setTasks([...tasks, { text: newTask }]);
+    event.target.reset();
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const handleEditTask = (index, newTask) => {
+    const newTasks = [...tasks];
+    newTasks[index].text = newTask;
+    setTasks(newTasks);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAddTask}>
+        <input type="text" name="task" placeholder="Add task" />
+        <button>Add</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task.text}
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+            <button
+              onClick={() =>
+                handleEditTask(
+                  index,
+                  prompt("Enter the new task text:", task.text)
+                )
+              }
+            >
+              Edit
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default ToDO;
+export default TodoList;
